049eed99728d77ea0c62f831838e74c3df11839f
teshe <teshomeabebe114@gmail.com>
2025-06-27 09:13:58 +0300

    Add UnknownUser icon and enhance WaitingComponent layout in Home

--- src/home.tsx
+++ src/home.tsx
@@ -1,5 +1,5 @@
 import { easeInOut, motion } from 'motion/react'
-import { Icon } from './components/Icon'
+import { Icon, UnknownUser } from './components/Icon'
 
 export default function Home() {
   return (
@@ -16,17 +16,55 @@ export default function Home() {
 
 function WaitingComponent() {
   return (
-    <div className="w-[400px] border-border bg-[#171819] text-[#B0B7BF] border h-full rounded-2xl">
-      <button className="px-4 py-2 bg-[#222425] rounded-2xl mt-10 ml-4">
-        <Icon name="ArrowLeft" />
-      </button>
-      <div className="border-border h-40 flex items-center justify-center border">
-        {/* Image placeholder*/} Image Placeholder
+    <div className="w-[400px] bg-[#171819] text-[#B0B7BF] border h-full rounded-2xl">
+      <div className="py-10 px-8">
+        <button className="px-4 py-2 bg-[#222425] hover:bg-[#2C2E30] transition-colors rounded-2xl">
+          <Icon name="ArrowLeft" />
+        </button>
+      </div>
+      <div className="h-40 flex items-center justify-center">
+        <img src="/dama.png" alt="ludo" className="object-contain" />
+      </div>
+      <Players />
+      <div className="px-4 flex justify-center">
+        <button className="px-4 py-2 flex gap-2 bg-[#222425] transition-colors hover:bg-[#2C2E30] items-center rounded-2xl">
+          <Icon name="ArrowLeft" /> <span className="">back to dama</span>
+        </button>
       </div>
     </div>
   )
 }
 
+function Players() {
+  return (
+    <div className="flex justify-between items-center px-16 py-6">
+      {/*  for the player that is ready */}
+      <div className="flex flex-col">
+        <Avatar />
+        <span className="text-base">Player</span>
+        <span className="font-medium text-[#7D8996]">Ready</span>
+      </div>
+      {/*  for the player that is waiting */}
+      <span className="text-4xl font-bold">VS</span>
+      {/*  for the player that is waiting */}
+      <div className="flex flex-col">
+        <div className="size-14 rounded-full bg-[#222425] flex items-center justify-center">
+          <UnknownUser />
+        </div>
+        <span className="text-base">Player</span>
+        <span className="font-medium text-[#7D8996]">Ready</span>
+      </div>
+    </div>
+  )
+}
+// this could Ideally be an Image with object-cover
+function Avatar() {
+  return (
+    <div className="size-14  rounded-full flex items-center justify-center bg-[#222425] font-bold text-lg">
+      T
+    </div>
+  )
+}
 function Loaders() {
   return (
     <div className="pt-10 flex flex-col gap-10">
